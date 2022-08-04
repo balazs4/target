8280702c9501e5d65069601e4712495bbc553ae3
balazs4 <balazs4@users.noreply.github.com>
2022-08-04 22:30:04 +0200

    skeleton

---
+++ index.test.js
@@ -0,0 +1,88 @@
+// const test = require('node:test');
+const assert = require('assert').strict;
+
+test('all good', (t) => {
+  const raw = [
+    'xxxxxxxxxxxxxxxxxxx',
+    'yyyyyyyyyyyyyyyyyyyyyyyyyyy',
+    'x_type_0',
+    'x_id_0',
+    'x_name_0',
+    JSON.stringify({
+      _from: 'previous',
+      foo: 'bar0',
+      bazz: 42,
+    }),
+    'x_type_1',
+    'x_id_1',
+    'x_name_1',
+    JSON.stringify({
+      _from: 'previous',
+      foo: 'bar1',
+      bazz: 42,
+    }),
+    'x_type_2',
+    'x_id_2',
+    'x_name_2',
+    JSON.stringify({
+      _from: 'next',
+      foo: 'bar2',
+      bazz: 42,
+    }),
+  ];
+
+  const headers = {
+    allx: 0,
+    ally: 1,
+    target_type_0: 2,
+    target_id_0: 3,
+    target_name_0: 4,
+    target_metadata_0: 5,
+    target_type_1: 6,
+    target_id_1: 7,
+    target_name_1: 8,
+    target_metadata_1: 9,
+    target_type_2: 10,
+    target_id_2: 11,
+    target_name_2: 12,
+    target_metadata_2: 13,
+  };
+
+  const expected = {
+    x: 'xxxxxxxxxxxxxxxxxxx',
+    y: 'yyyyyyyyyyyyyyyyyyyyyyyyyyy',
+    previous: {
+      x_type_0: {
+        name: 'x_name_0',
+        id: 'x_id_0',
+        foo: 'bar0',
+        bazz: 42,
+      },
+      x_type_1: {
+        name: 'x_name_1',
+        id: 'x_id_1',
+        foo: 'bar1',
+        bazz: 42,
+      },
+    },
+    next: {
+      x_type_2: {
+        name: 'x_name_2',
+        id: 'x_id_2',
+        foo: 'bar2',
+        bazz: 42,
+      },
+    },
+  };
+
+  const actual = (() => {
+    return {
+      x: raw[headers['allx']],
+      y: raw[headers['ally']],
+      previous: {},
+      next: {},
+    };
+  })();
+
+  assert.deepEqual(actual, expected);
+});
